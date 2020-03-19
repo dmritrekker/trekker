2db7305a4cba6fd522507ce01977ac7a01ebfb21
Baran Aydogan <baran.aydogan@gmail.com>
2020-03-19 12:25:40 +0200

    improved build script

--- build_Linux.sh
+++ build_Linux.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 cmakeExe=/home/baran/apps/cmake-3.15.0-Linux-x86_64/bin/cmake
+buildType=Release #Release or Debug
+
+c_compiler=/usr/bin/gcc
+cxx_compiler=/usr/bin/g++
+
 buildPythonPackage=ON #ON or OFF
 pythonExe=/home/baran/apps/anaconda3/envs/trekkerWrapper/bin/python
-buildType=Release #Release or Debug
+
 
 rm -rf build/Linux
 mkdir -p build/Linux
 cd build/Linux
-${cmakeExe} -DCMAKE_BUILD_TYPE=${buildType} -DBuild_Python3_WRAPPER=${buildPythonPackage} -DPython3_EXECUTABLE=${pythonExe} ../..
+${cmakeExe} -DCMAKE_CC_COMPILER=${c_compiler} -DCMAKE_CXX_COMPILER=${cxx_compiler} -DCMAKE_BUILD_TYPE=${buildType} -DBuild_Python3_WRAPPER=${buildPythonPackage} -DPython3_EXECUTABLE=${pythonExe} ../..
 ${cmakeExe} --build . --config ${buildType} --target install --parallel 8
 cd ../..
