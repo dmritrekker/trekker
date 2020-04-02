3939409a0abf7cb68aff3f7da9efb7eb4373c178
Baran Aydogan <baran.aydogan@gmail.com>
2020-04-02 15:56:02 +0300

    demo for windows added

---
+++ demo/win_demo.bat
@@ -0,0 +1,33 @@
+@SET FOD="data\FOD.nii"
+@SET SEEDimag="data\seedImage.nii"
+@SET SEEDcoor="data\seedCoordinates.txt"
+
+@echo =================================
+@echo DEMO 1: Seed image based tracking
+@echo =================================
+..\binaries\trekker_win_x64_v0.5.exe ^
+-fod %FOD% ^
+-seed_image %SEEDimag% ^
+-seed_count 100 ^
+-enableOutputOverwrite ^
+-output win_demo_output1 ^
+-verboseLevel 0 ^
+-writeColors
+
+@echo:
+@echo:
+@echo ======================================
+@echo DEMO 2: Seed coordinate based tracking
+@echo ======================================
+..\binaries\trekker_win_x64_v0.5.exe ^
+-fod %FOD% ^
+-seed_coordinates %SEEDcoor% ^
+-enableOutputOverwrite ^
+-output win_demo_output2 ^
+-verboseLevel 0 ^
+-writeColors
+
+@echo:
+@echo ======================================
+@echo If you can see 'win_demo_output1' and 'win_demo_output2' in the demo folder, all is good.
+@echo ======================================
