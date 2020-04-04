98f87a90ce2c8a1b0939edfbe25d9f00ce9e7996
Baran Aydogan <baran.aydogan@gmail.com>
2020-04-04 11:31:02 +0300

    updated windows binaries
    ...also revert back to cmake v3.15 since windows complaint about the older version

--- build_Windows.bat
+++ build_Windows.bat
@@ -1,11 +1,11 @@
-@SET cmakeExe="C:\Program Files\CMake\bin\cmake"
+@SET cmakeExe="C:/Program Files/CMake/bin/cmake"
 @SET buildPythonPackage=ON
-@SET pythonExe="C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\python"
+@SET pythonExe="C:/Program Files (x86)/Microsoft Visual Studio/Shared/Python37_64/python"
 @SET buildType=Release
 
 @rmdir build\Windows /s /q
 @mkdir build\Windows
 @cd build\Windows
-@%cmakeExe% {-D Python3_EXECUTABLE=%pythonExe%} -DBuild_Python3_WRAPPER=%buildPythonPackage% ../..
+@%cmakeExe% -DPython3_EXECUTABLE=%pythonExe% -DBuild_Python3_WRAPPER=%buildPythonPackage% ../..
 @%cmakeExe% --build . --config %buildType% --target install --parallel 8
 @cd ../..
