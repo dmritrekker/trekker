3c9278b33c9bdff77704354023a8eabcc1a5961a
Baran Aydogan <baran.aydogan@gmail.com>
2020-05-22 22:20:34 +0300

    remove build

--- build/Linux/CMakeFiles/Trekker.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libTrekker.pdb"
-  "libTrekker.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C CXX)
-  include(CMakeFiles/Trekker.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
