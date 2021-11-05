bf8cdb755ff889baafedc5f16f11af4f46bb249e
Baran Aydogan <baran.aydogan@gmail.com>
2021-11-05 13:13:11 +0200

    repo clean-up

--- build/CMakeFiles/tmp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libtmp.a"
-  "libtmp.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C CXX)
-  include(CMakeFiles/tmp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
