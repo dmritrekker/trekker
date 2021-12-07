352a55d0d1a20a1465d55ef347275e8b8802a48b
baranaydogan <baran.aydogan@gmail.com>
2021-12-07 23:36:47 +0200

    v0.9

--- build_Linux.sh
+++ build_Linux.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-cmakeExe=/home/baran/apps/cmake-3.15.0-Linux-x86_64/bin/cmake
+cmakeExe=cmake
 buildType=Release #Release or Debug
 
-c_compiler=gcc-9
-cxx_compiler=g++-9
+c_compiler=gcc
+cxx_compiler=g++
 
 buildPythonPackage=ON #ON or OFF
-pythonExe=/home/baran/apps/anaconda3/envs/trekkerWrapper_py38/bin/python
-
-
+pythonExe=python
 
 rm -rf build/Linux
 mkdir -p build/Linux
 cd build/Linux
 ${cmakeExe} -DCMAKE_C_COMPILER=${c_compiler} -DCMAKE_CXX_COMPILER=${cxx_compiler} -DCMAKE_BUILD_TYPE=${buildType} -DBuild_Python3_WRAPPER=${buildPythonPackage} -DPython3_EXECUTABLE=${pythonExe} ../..
-${cmakeExe} --build . --config ${buildType} --target install --parallel 8
+${cmakeExe} --build . --config ${buildType} --target install --parallel 16
 cd ../..
