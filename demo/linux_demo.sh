c8321b63071733bf2fd066a86c3360fc86920262
baranaydogan <baran.aydogan@gmail.com>
2024-05-17 12:18:25 +0300

    fixed demo for v1.0b

--- demo/linux_demo.sh
+++ demo/linux_demo.sh
@@ -4,27 +4,12 @@ FOD=data/FOD.nii
 SEEDimag=data/seedImage.nii
 SEEDcoor=data/seedCoordinates.txt
 
-echo "================================="
-echo "DEMO 1: Seed image based tracking"
-echo "================================="
-../binaries/trekker_linux_x64_v0.9 \
--fod ${FOD} \
--seed_image ${SEEDimag} \
--seed_count 100 \
--enableOutputOverwrite \
--output linux_demo_output1 \
--writeColors
+trekker=../releases/v1.0b/trekker_linux_x64_v1.0b
 
-echo "\n\n======================================"
-echo "DEMO 2: Seed coordinate based tracking"
-echo "======================================"
-../binaries/trekker_linux_x64_v0.9 \
--fod ${FOD} \
--seed_coordinates ${SEEDcoor} \
--seed_count 100 \
--enableOutputOverwrite \
--output linux_demo_output2 \
--writeColors
-echo "======================================"
-echo "If you can see 'linux_demo_output1' and 'linux_demo_output2' in the demo folder, all is good."
-echo "======================================"
+${trekker} \
+--input ${FOD} \
+--output linux_demo_output.vtk \
+--seed ${SEEDimag} \
+--seed_count 100 \
+--force
+
