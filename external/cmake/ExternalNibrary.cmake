7afa85d900e725a4c6aec8fa66c765e8902fe2c9
baranaydogan <baran.aydogan@gmail.com>
2025-01-13 13:59:45 +0200

    mod OpenMP ext nibrary

--- external/cmake/ExternalNibrary.cmake
+++ external/cmake/ExternalNibrary.cmake
@@ -64,6 +64,11 @@ if (NOT USE_SYSTEM_NIBRARY)
     if(BUILDING_NIBRARY_FROM_SOURCE)
 
         include(ExternalProject)
+
+        # Set OpenMP flags as cache variables
+        set(OpenMP_C_FLAGS "$ENV{OpenMP_C_FLAGS}" CACHE STRING "OpenMP C compiler flags" FORCE)
+        set(OpenMP_CXX_FLAGS "$ENV{OpenMP_CXX_FLAGS}" CACHE STRING "OpenMP CXX compiler flags" FORCE)
+
         ExternalProject_Add(build_nibrary
 
             SOURCE_DIR "${NIBRARY_SOURCE_DIR}"
@@ -74,12 +79,12 @@ if (NOT USE_SYSTEM_NIBRARY)
                         -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
                         -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
                         -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-                        -DOpenMP_C_FLAGS=$ENV{OpenMP_C_FLAGS}
-                        -DOpenMP_CXX_FLAGS=$ENV{OpenMP_CXX_FLAGS}
-                        -DOpenMP_C_LIB_NAMES=$ENV{OpenMP_C_LIB_NAMES}
-                        -DOpenMP_CXX_LIB_NAMES=$ENV{OpenMP_CXX_LIB_NAMES}
-                        -DOpenMP_omp_LIBRARY=$ENV{OpenMP_omp_LIBRARY}
+        )
 
+        # Set properties AFTER ExternalProject_Add
+        set_target_properties(build_nibrary PROPERTIES
+            IMPORTED_LINK_INTERFACE_LIBRARIES "${OpenMP_CXX_LIB_NAMES}"
+            IMPORTED_COMPILE_OPTIONS "${OpenMP_CXX_FLAGS}"
         )
 
         ExternalProject_Add_Step(build_nibrary POST_BUILD
