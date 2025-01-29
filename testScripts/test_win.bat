cc7fd5801a54df79baf3d7313b9970e7af9162fa
baranaydogan <baran.aydogan@gmail.com>
2025-01-29 11:41:34 +0200

    prepping test

--- testScripts/test_win.bat
+++ testScripts/test_win.bat
@@ -1,10 +1,13 @@
 @echo off
 
+@REM Get the directory of the current batch file
+set "batchDir=%~dp0"
+
 @REM Display help
-call .\trekker_win.exe
+call "%batchDir%trekker_win.exe"
 
 @REM Run a simple fiber tracking script
-call .\trekker_win.exe track -f ^
+call "%batchDir%trekker_win.exe" track -f ^
 ..\testData\100307_FOD_Order4.nii.gz ^
 --seed ..\testData\100307_lh_white.vtk ^
 --seed_count 100 ^
@@ -14,14 +17,14 @@ call .\trekker_win.exe track -f ^
 if exist "out_win.vtk" (
     echo out_win.vtk exists.
     for %%a in ("out_win.vtk") do echo File size: %%~za bytes
-    call .\trekker_win.exe info out_win.vtk
+    call "%batchDir%trekker_win.exe" info out_win.vtk
 ) else (
     echo out_win.vtk does not exist.
 )
 
 @REM Display info help
-call .\trekker_win.exe info
+call "%batchDir%trekker_win.exe" info
 
 @REM Display track help
-call .\trekker_win.exe track
+call "%batchDir%trekker_win.exe" track
 
