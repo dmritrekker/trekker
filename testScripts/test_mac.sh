e42ee61f73212d73b057ee23b0b87ace248e40fa
baranaydogan <baran.aydogan@gmail.com>
2025-01-30 09:29:47 +0200

    macos command parse test

--- testScripts/test_mac.sh
+++ testScripts/test_mac.sh
@@ -20,14 +20,19 @@ export OpenMP_omp_LIBRARY="${libomp_prefix}/lib/libomp.dylib"
 chmod +x ./trekker_mac
 
 # Display help
+echo "Testing help"
 ./trekker_mac
+echo "Done"
 
 # Run a simple fiber tracking script
+echo "Run a simple fiber tracking script"
 ./trekker_mac track -f \
 ../testData/100307_FOD_Order4.nii.gz \
 --seed ../testData/100307_lh_white.vtk \
 --seed_count 100 \
 --output out.vtk
+echo "Done"
+
 
 # Display tractogram info
 if [ -f "out.vtk" ]; then
@@ -38,14 +43,14 @@ else
     echo "out.vtk does not exist."
 fi
 
-echo "Testing info help"
 # Display info help
+echo "Testing info help"
 ./trekker_mac info
 echo "Done"
 
 
-echo "Testing track help"
 # Display track help
+echo "Testing track help"
 ./trekker_mac track
 echo "Done"
 
