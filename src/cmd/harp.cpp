760f45ef62d141022e4d41fb12afc7e8485d1dad
baranaydogan <baran.aydogan@gmail.com>
2026-05-04 12:05:01 +0300

    harp

--- src/cmd/harp.cpp
+++ src/cmd/harp.cpp
@@ -58,9 +58,9 @@ typedef enum {
 // The next 10 classes are L2 implausibility classes.
 // The rest of the 21 classes are L2 plausible classes, and they depict which two regions the streamline connects.
 typedef enum {
-    L1_implausible_CSF_entry = 0,
+    L1_implausible_CSF_entry_and_WM_termination = 0,
+    L1_implausible_CSF_entry,
     L1_implausible_WM_termination,
-    L1_implausible_CSF_entry_and_WM_termination,
     L2_implausible_trj1,
     L2_implausible_trj2,
     L2_implausible_trj3,
@@ -93,6 +93,49 @@ typedef enum {
     L2_CER_GM___BS,
     L2_BS___BS
 } Streamline_Class_Detailed;
+
+// Logger
+std::atomic<std::size_t> log_L1_implausible{0};
+std::atomic<std::size_t> log_L1_plausible{0};
+std::atomic<std::size_t> log_L2_implausible{0};
+std::atomic<std::size_t> log_L2_plausible{0};
+
+std::atomic<std::size_t> log_L1_implausible_CSF_entry_and_WM_termination{0};
+std::atomic<std::size_t> log_L1_implausible_CSF_entry{0};
+std::atomic<std::size_t> log_L1_implausible_WM_termination{0};
+
+std::atomic<std::size_t> log_L2_implausible_trj1{0};
+std::atomic<std::size_t> log_L2_implausible_trj2{0};
+std::atomic<std::size_t> log_L2_implausible_trj3{0};
+std::atomic<std::size_t> log_L2_implausible_trj4{0};
+std::atomic<std::size_t> log_L2_implausible_trj5{0};
+std::atomic<std::size_t> log_L2_implausible_trj6{0};
+std::atomic<std::size_t> log_L2_implausible_trj7{0};
+std::atomic<std::size_t> log_L2_implausible_trj8{0};
+std::atomic<std::size_t> log_L2_implausible_trj9{0};
+std::atomic<std::size_t> log_L2_implausible_trj10{0};
+
+std::atomic<std::size_t> log_L2_LN_GM___LN_GM{0};
+std::atomic<std::size_t> log_L2_LN_GM___RN_GM{0};
+std::atomic<std::size_t> log_L2_LN_GM___L_SUB{0};
+std::atomic<std::size_t> log_L2_LN_GM___R_SUB{0};
+std::atomic<std::size_t> log_L2_LN_GM___CER_GM{0};
+std::atomic<std::size_t> log_L2_LN_GM___BS{0};
+std::atomic<std::size_t> log_L2_RN_GM___RN_GM{0};
+std::atomic<std::size_t> log_L2_RN_GM___L_SUB{0};
+std::atomic<std::size_t> log_L2_RN_GM___R_SUB{0};
+std::atomic<std::size_t> log_L2_RN_GM___CER_GM{0};
+std::atomic<std::size_t> log_L2_RN_GM___BS{0};
+std::atomic<std::size_t> log_L2_L_SUB___L_SUB{0};
+std::atomic<std::size_t> log_L2_L_SUB___R_SUB{0};
+std::atomic<std::size_t> log_L2_L_SUB___CER_GM{0};
+std::atomic<std::size_t> log_L2_L_SUB___BS{0};
+std::atomic<std::size_t> log_L2_R_SUB___R_SUB{0};
+std::atomic<std::size_t> log_L2_R_SUB___CER_GM{0};
+std::atomic<std::size_t> log_L2_R_SUB___BS{0};
+std::atomic<std::size_t> log_L2_CER_GM___CER_GM{0};
+std::atomic<std::size_t> log_L2_CER_GM___BS{0};
+std::atomic<std::size_t> log_L2_BS___BS{0};
 
 // Helper function to write a boolean matrix to a CSV file
 bool write_matrix_to_csv(const std::string& filename, const std::vector<std::vector<bool>>& data) {
@@ -227,6 +270,60 @@ void run_harp()
         }
     }
 
+    // Helper function for printing progress
+
+    auto printProgress = [&]()
+{
+        // Print out the statistics
+        std::size_t total_analyzed = log_L1_implausible.load() + log_L2_implausible.load() + log_L2_plausible.load();
+
+        std::cout << std::endl;
+        disp(MSG_INFO, "Total number of analyzed streamlines / all streamlines: %zu / %zu", total_analyzed, tractogram.numberOfStreamlines);
+        std::cout << std::endl;
+        disp(MSG_INFO, "L1 implausible: %zu", log_L1_implausible.load());
+        disp(MSG_INFO, "L1 plausible:   %zu", log_L1_plausible.load());
+        disp(MSG_INFO, "L2 implausible: %zu", log_L2_implausible.load());
+        disp(MSG_INFO, "L2 plausible:   %zu", log_L2_plausible.load());
+        std::cout << std::endl;
+        disp(MSG_INFO, "L1 implausible - CSF entry and WM termination:  %zu", log_L1_implausible_CSF_entry_and_WM_termination.load());
+        disp(MSG_INFO, "L1 implausible - CSF entry:                     %zu", log_L1_implausible_CSF_entry.load());
+        disp(MSG_INFO, "L1 implausible - WM termination:                %zu", log_L1_implausible_WM_termination.load());
+        std::cout << std::endl;
+        disp(MSG_INFO, "L2 implausible - trj1:  %zu", log_L2_implausible_trj1.load());
+        disp(MSG_INFO, "L2 implausible - trj2:  %zu", log_L2_implausible_trj2.load());
+        disp(MSG_INFO, "L2 implausible - trj3:  %zu", log_L2_implausible_trj3.load());
+        disp(MSG_INFO, "L2 implausible - trj4:  %zu", log_L2_implausible_trj4.load());
+        disp(MSG_INFO, "L2 implausible - trj5:  %zu", log_L2_implausible_trj5.load());
+        disp(MSG_INFO, "L2 implausible - trj6:  %zu", log_L2_implausible_trj6.load());
+        disp(MSG_INFO, "L2 implausible - trj7:  %zu", log_L2_implausible_trj7.load());
+        disp(MSG_INFO, "L2 implausible - trj8:  %zu", log_L2_implausible_trj8.load());
+        disp(MSG_INFO, "L2 implausible - trj9:  %zu", log_L2_implausible_trj9.load());
+        disp(MSG_INFO, "L2 implausible - trj10: %zu", log_L2_implausible_trj10.load());
+        std::cout << std::endl;
+        disp(MSG_INFO, "L2 plausible - LN_GM___LN_GM:   %zu", log_L2_LN_GM___LN_GM.load());
+        disp(MSG_INFO, "L2 plausible - LN_GM___RN_GM:   %zu", log_L2_LN_GM___RN_GM.load());
+        disp(MSG_INFO, "L2 plausible - LN_GM___L_SUB:   %zu", log_L2_LN_GM___L_SUB.load());
+        disp(MSG_INFO, "L2 plausible - LN_GM___R_SUB:   %zu", log_L2_LN_GM___R_SUB.load());
+        disp(MSG_INFO, "L2 plausible - LN_GM___CER_GM:  %zu", log_L2_LN_GM___CER_GM.load());
+        disp(MSG_INFO, "L2 plausible - LN_GM___BS:      %zu", log_L2_LN_GM___BS.load());
+        disp(MSG_INFO, "L2 plausible - RN_GM___RN_GM:   %zu", log_L2_RN_GM___RN_GM.load());
+        disp(MSG_INFO, "L2 plausible - RN_GM___L_SUB:   %zu", log_L2_RN_GM___L_SUB.load());
+        disp(MSG_INFO, "L2 plausible - RN_GM___R_SUB:   %zu", log_L2_RN_GM___R_SUB.load());
+        disp(MSG_INFO, "L2 plausible - RN_GM___CER_GM:  %zu", log_L2_RN_GM___CER_GM.load());
+        disp(MSG_INFO, "L2 plausible - RN_GM___BS:      %zu", log_L2_RN_GM___BS.load());
+        disp(MSG_INFO, "L2 plausible - L_SUB___L_SUB:   %zu", log_L2_L_SUB___L_SUB.load());
+        disp(MSG_INFO, "L2 plausible - L_SUB___R_SUB:   %zu", log_L2_L_SUB___R_SUB.load());
+        disp(MSG_INFO, "L2 plausible - L_SUB___CER_GM:  %zu", log_L2_L_SUB___CER_GM.load());
+        disp(MSG_INFO, "L2 plausible - L_SUB___BS:      %zu", log_L2_L_SUB___BS.load());
+        disp(MSG_INFO, "L2 plausible - R_SUB___R_SUB:   %zu", log_L2_R_SUB___R_SUB.load());
+        disp(MSG_INFO, "L2 plausible - R_SUB___CER_GM:  %zu", log_L2_R_SUB___CER_GM.load());
+        disp(MSG_INFO, "L2 plausible - R_SUB___BS:      %zu", log_L2_R_SUB___BS.load());
+        disp(MSG_INFO, "L2 plausible - CER_GM___CER_GM: %zu", log_L2_CER_GM___CER_GM.load());
+        disp(MSG_INFO, "L2 plausible - CER_GM___BS:     %zu", log_L2_CER_GM___BS.load());
+        disp(MSG_INFO, "L2 plausible - BS___BS:         %zu", log_L2_BS___BS.load());
+        std::cout << std::endl;
+    };
+
     // Filter streamline in batches
     int batch_size_max  = 50000;
     int batch_start_idx = 0;
@@ -276,25 +373,124 @@ void run_harp()
             
             // All pathways are processed by this point.
             // We can now clear ambiguities and assign the class for this streamline.
-
-            // We start by L1 status. 
-
             const auto& stat = batch_stat[task.no];
+            
+            bool isImplausible = false;
 
+            auto assignImplausibleClass = [&](Streamline_Class_Detailed impl_class) {
+                detailedClass[batch_start_idx + task.no] = impl_class; 
+                isImplausible     = true;
+            };
+
+        
+            // L1 class assignment
             {
-                // L1 implausibility
-                bool implausible = false;
-                if (stat[enters_CSF])                   {detailedClass[batch_start_idx + task.no] = L1_implausible_CSF_entry;                    implausible = true;}
-                if (!stat[L1_ends])                     {detailedClass[batch_start_idx + task.no] = L1_implausible_WM_termination;               implausible = true;}
-                if (stat[enters_CSF] && !stat[L1_ends]) {detailedClass[batch_start_idx + task.no] = L1_implausible_CSF_entry_and_WM_termination; implausible = true;}
+                if (!isImplausible && stat[enters_CSF] && !stat[L1_ends]) {
+                    assignImplausibleClass(L1_implausible_CSF_entry_and_WM_termination);
+                    log_L1_implausible_CSF_entry_and_WM_termination.fetch_add(1);
+                }
+
+                if (!isImplausible && stat[enters_CSF]) {
+                    assignImplausibleClass(L1_implausible_CSF_entry);
+                    log_L1_implausible_CSF_entry.fetch_add(1);
+                }
                 
-                courseClass[batch_start_idx + task.no] = (implausible) ? L1_implausible : L1_plausible;
+                if (!isImplausible && !stat[L1_ends]) {
+                    assignImplausibleClass(L1_implausible_WM_termination);
+                    log_L1_implausible_WM_termination.fetch_add(1);
+                }
+
+                if (isImplausible) {
+                    courseClass[batch_start_idx + task.no] = L1_implausible;
+                    log_L1_implausible.fetch_add(1);
+                } else {
+                    courseClass[batch_start_idx + task.no] = L1_plausible;
+                    log_L1_plausible.fetch_add(1);
+                }
+
             }
 
-            {
-                // L2 implausibility
-                bool implausible = false;
+            // If L1 plausible, then we assign an L2 class.
+            if (!isImplausible) {
                 
+                if (!isImplausible && stat[RN_GM___RN_GM] && stat[enters_LN_WM]) {
+                    assignImplausibleClass(L2_implausible_trj1);
+                    log_L2_implausible_trj1.fetch_add(1);
+                }
+                if (!isImplausible && stat[LN_GM___LN_GM] && stat[enters_RN_WM]) {
+                    assignImplausibleClass(L2_implausible_trj2);
+                    log_L2_implausible_trj2.fetch_add(1);
+                }
+                if (!isImplausible && stat[LN_GM___BS] && stat[enters_RN_WM]) {
+                    assignImplausibleClass(L2_implausible_trj3);
+                    log_L2_implausible_trj3.fetch_add(1);
+                }
+                if (!isImplausible && stat[RN_GM___BS] && stat[enters_LN_WM]) {
+                    assignImplausibleClass(L2_implausible_trj4);
+                    log_L2_implausible_trj4.fetch_add(1);
+                }
+                if (!isImplausible && stat[RN_GM___RN_GM] && stat[enters_BS]) {
+                    assignImplausibleClass(L2_implausible_trj5);
+                    log_L2_implausible_trj5.fetch_add(1);
+                }
+                if (!isImplausible && stat[LN_GM___LN_GM] && stat[enters_BS]) {
+                    assignImplausibleClass(L2_implausible_trj6);
+                    log_L2_implausible_trj6.fetch_add(1);
+                }
+                if (!isImplausible && stat[LN_GM___RN_GM] && stat[enters_BS]) {
+                    assignImplausibleClass(L2_implausible_trj7);
+                    log_L2_implausible_trj7.fetch_add(1);
+                }
+                if (!isImplausible && stat[BS___BS] && stat[enters_LN_WM]) {
+                    assignImplausibleClass(L2_implausible_trj8);
+                    log_L2_implausible_trj8.fetch_add(1);
+                }
+                if (!isImplausible && stat[BS___BS] && stat[enters_RN_WM]) {
+                    assignImplausibleClass(L2_implausible_trj9);
+                    log_L2_implausible_trj9.fetch_add(1);
+                }
+                if (!isImplausible && stat[BS___BS] && stat[enters_CER_WM]) {
+                    assignImplausibleClass(L2_implausible_trj10);
+                    log_L2_implausible_trj10.fetch_add(1);
+                }
+
+                // The rest of the classes are mutually exclusive.
+                if (isImplausible) {
+                    courseClass[batch_start_idx + task.no] = L2_implausible;
+                    log_L2_implausible.fetch_add(1);
+                } else {
+                    courseClass[batch_start_idx + task.no] = L2_plausible;
+                    log_L2_plausible.fetch_add(1);
+
+                    // Assigning detailed class for L2 plausible streamlines.
+                    for (int i = LN_GM___LN_GM; i < NUM_PATHWAYS; i++) {
+                        if (stat[i]) {
+                            switch (i) {
+                                case LN_GM___LN_GM:   detailedClass[batch_start_idx + task.no] = L2_LN_GM___LN_GM;   log_L2_LN_GM___LN_GM.fetch_add(1);   break;
+                                case LN_GM___RN_GM:   detailedClass[batch_start_idx + task.no] = L2_LN_GM___RN_GM;   log_L2_LN_GM___RN_GM.fetch_add(1);   break;
+                                case LN_GM___L_SUB:   detailedClass[batch_start_idx + task.no] = L2_LN_GM___L_SUB;   log_L2_LN_GM___L_SUB.fetch_add(1);   break;
+                                case LN_GM___R_SUB:   detailedClass[batch_start_idx + task.no] = L2_LN_GM___R_SUB;   log_L2_LN_GM___R_SUB.fetch_add(1);   break;
+                                case LN_GM___CER_GM:  detailedClass[batch_start_idx + task.no] = L2_LN_GM___CER_GM;  log_L2_LN_GM___CER_GM.fetch_add(1);  break;
+                                case LN_GM___BS:      detailedClass[batch_start_idx + task.no] = L2_LN_GM___BS;      log_L2_LN_GM___BS.fetch_add(1);      break;
+                                case RN_GM___RN_GM:   detailedClass[batch_start_idx + task.no] = L2_RN_GM___RN_GM;   log_L2_RN_GM___RN_GM.fetch_add(1);   break;
+                                case RN_GM___L_SUB:   detailedClass[batch_start_idx + task.no] = L2_RN_GM___L_SUB;   log_L2_RN_GM___L_SUB.fetch_add(1);   break;
+                                case RN_GM___R_SUB:   detailedClass[batch_start_idx + task.no] = L2_RN_GM___R_SUB;   log_L2_RN_GM___R_SUB.fetch_add(1);   break;
+                                case RN_GM___CER_GM:  detailedClass[batch_start_idx + task.no] = L2_RN_GM___CER_GM;  log_L2_RN_GM___CER_GM.fetch_add(1);  break;
+                                case RN_GM___BS:      detailedClass[batch_start_idx + task.no] = L2_RN_GM___BS;      log_L2_RN_GM___BS.fetch_add(1);      break;
+                                case L_SUB___L_SUB:   detailedClass[batch_start_idx + task.no] = L2_L_SUB___L_SUB;   log_L2_L_SUB___L_SUB.fetch_add(1);   break;
+                                case L_SUB___R_SUB:   detailedClass[batch_start_idx + task.no] = L2_L_SUB___R_SUB;   log_L2_L_SUB___R_SUB.fetch_add(1);   break;
+                                case L_SUB___CER_GM:  detailedClass[batch_start_idx + task.no] = L2_L_SUB___CER_GM;  log_L2_L_SUB___CER_GM.fetch_add(1);  break;
+                                case L_SUB___BS:      detailedClass[batch_start_idx + task.no] = L2_L_SUB___BS;      log_L2_L_SUB___BS.fetch_add(1);      break;
+                                case R_SUB___R_SUB:   detailedClass[batch_start_idx + task.no] = L2_R_SUB___R_SUB;   log_L2_R_SUB___R_SUB.fetch_add(1);   break;
+                                case R_SUB___CER_GM:  detailedClass[batch_start_idx + task.no] = L2_R_SUB___CER_GM;  log_L2_R_SUB___CER_GM.fetch_add(1);  break;
+                                case R_SUB___BS:      detailedClass[batch_start_idx + task.no] = L2_R_SUB___BS;      log_L2_R_SUB___BS.fetch_add(1);      break;
+                                case CER_GM___CER_GM: detailedClass[batch_start_idx + task.no] = L2_CER_GM___CER_GM; log_L2_CER_GM___CER_GM.fetch_add(1); break;
+                                case CER_GM___BS:     detailedClass[batch_start_idx + task.no] = L2_CER_GM___BS;     log_L2_CER_GM___BS.fetch_add(1);     break;
+                                case BS___BS:         detailedClass[batch_start_idx + task.no] = L2_BS___BS;         log_L2_BS___BS.fetch_add(1);         break;
+                            }
+                        }
+                    }
+                }
 
             }
             
@@ -310,9 +506,15 @@ void run_harp()
             }
         }
 
+        printProgress();
+        std::cout << "\033[45F";
+
         batch_start_idx += batch_size;
         
     }
+
+    
+    printProgress();
 
 
     // Write the output as a .csv file
@@ -345,7 +547,7 @@ void harp(CLI::App* app)
         ->required()
         ->check(CLI::ExistingFile);
 
-    app->add_option ("--output_table",       out_table,          "A .csv table marking the streamlines and matching pathways");
+    app->add_option ("--out_table",          out_table,          "A .csv table marking the streamlines and matching pathways");
 
     app->add_option("--numberOfThreads, -n", numberOfThreads,    "Number of threads.");
     app->add_option("--verbose, -v",         verbose,            "Verbose level. Options are \"quiet\",\"fatal\",\"error\",\"warn\",\"info\" and \"debug\". Default=info");
